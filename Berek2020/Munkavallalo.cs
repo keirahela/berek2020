cd917275acb29d386ff593b435dc8be184aadd0b
Kósa Márk Milán <kosa.mark@verebely.dc>
2024-12-16 08:48:35 +0100

    initial commit

---
+++ Berek2020/Munkavallalo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Berek2020
+{
+    internal class Munkavallalo
+    {
+        private string munkavallaloNev;
+        private bool ferfi;
+        private string reszleg;
+        private int belepesEv;
+        private int ber;
+
+        public Munkavallalo(string adatSor)
+        {
+            string[] mezok = adatSor.Split(';');
+            munkavallaloNev = mezok[0];
+            ferfi = mezok[1] == "férfi";
+            reszleg = mezok[2];
+            belepesEv = int.Parse(mezok[3]);
+            ber = int.Parse(mezok[4]);
+        }
+
+        public string Nev
+        {
+            get => munkavallaloNev;
+            set => munkavallaloNev = value;
+        }
+
+        public bool Neme
+        {
+            get => ferfi;
+            set => ferfi = value;
+        }
+
+        public string Reszleg
+        {
+            get => reszleg;
+            set => reszleg = value;
+        }
+
+        public int BelepesEv
+        {
+            get => belepesEv;
+            set => belepesEv = value;
+        }
+
+        public int Ber
+        {
+            get => ber;
+            set => ber = value;
+        }
+    }
+}
---
+++ Berek2020/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Berek2020
+{
+    internal class FoProgram
+    {
+        static void Main(string[] args)
+        {
+            List<Munkavallalo> munkavallaloLista = new List<Munkavallalo>();
+            using StreamReader olvaso = new StreamReader(path: "berek2020.txt", Encoding.UTF8);
+            olvaso.ReadLine();
+            while (!olvaso.EndOfStream)
+            {
+                munkavallaloLista.Add(new Munkavallalo(olvaso.ReadLine()));
+            }
+
+            Console.WriteLine($"3. Feladat: Munkavállalók száma: {munkavallaloLista.Count} fő");
+
+            var atlagBer = munkavallaloLista.Average(e => e.Ber);
+            Console.WriteLine($"4. Feladat: {Math.Round((atlagBer / 1000), 1)} ezer Ft");
+
+            Console.Write($"5. Feladat: Kérem adja meg a részleg nevét: ");
+            string reszlegInput = Console.ReadLine().ToLower();
+            var legmagasabbBeruMunkavallalo = munkavallaloLista
+                .Where(emp => emp.Reszleg == reszlegInput)
+                .OrderByDescending(emp => emp.Ber)
+                .FirstOrDefault();
+
+            if (legmagasabbBeruMunkavallalo != null)
+            {
+                Console.WriteLine($"6. Feladat: A megadott részleg legmagasabb bérű munkavállalója:" +
+                    $"\n\tNév: {legmagasabbBeruMunkavallalo.Nev}\n\tNeme: {(legmagasabbBeruMunkavallalo.Neme ? "férfi" : "nő")}" +
+                    $"\n\tBelépés Dátuma: {legmagasabbBeruMunkavallalo.BelepesEv}\n\tBér: {legmagasabbBeruMunkavallalo.Ber:000 000} Ft");
+            }
+            else
+            {
+                Console.WriteLine($"6. Feladat: A megadott részleg nem létezik a cégnél!");
+            }
+
+            var reszlegStatisztika = munkavallaloLista.GroupBy(emp => emp.Reszleg).OrderBy(csoport => csoport.Key);
+            Console.WriteLine("7. Feladat: Statisztika");
+            foreach (var csoport in reszlegStatisztika)
+            {
+                Console.WriteLine($"\t{csoport.Key} - {csoport.Count()} Munkavállaló");
+            }
+        }
+    }
+}
